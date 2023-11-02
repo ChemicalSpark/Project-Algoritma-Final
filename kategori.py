7f732d496be5b0b81174e0519dec3d6bfb82cf43
Oujireflect <oujireflect.me@gmail.com>
2023-11-02 15:25:12 +0700

    Melanjutkan tambah kategori

--- kategori.py
+++ kategori.py
@@ -2,16 +2,31 @@ import csv
 import pandas
 
 def list_kategori():
-    with open('database/kategori.csv', 'r') as db_kategori:
+    with open('Project-Algoritma-Final/database/kategori.csv', 'r') as db_kategori:
         read = db_kategori.read()
         print(read)
 
 def tambah_kategori():
+    id_kategori = 1
     print("Tambahkan Kategori!")
     add_kategori = input("Masukkan kategori: ")
-    pass
+    with open('Project-Algoritma-Final/database/kategori.csv', 'a') as write_kategori:
+        write_kategori.write(add_kategori)
+        
 
 def hapus_kategori():
     pass
 
-list_kategori()
+print("[KATEGORI]")
+print("Menu: ")
+print("""1. List Kategori
+2. Tambah Kategori
+""")
+user = int(input("Pilihan: "))
+match user:
+    case 1:
+        list_kategori()
+    case 2:
+        tambah_kategori()
+    case 3:
+        pass
