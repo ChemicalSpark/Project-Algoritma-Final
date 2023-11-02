bc6da1a3d065d2bcea5d19a2e5b766189aa22b34
Rangg <142792905+ChemicalSpark@users.noreply.github.com>
2023-11-02 15:58:50 +0700

    Update core.py

--- core.py
+++ core.py
@@ -1,42 +1,48 @@
-
-def perbarui_csv(nama_file, data, indeks_baris, baris_baru):
-    if 0 <= indeks_baris < len(data):
-        data[indeks_baris] = baris_baru
-
-        file_csv = open(nama_file, 'w')
-
-        for baris in data:
-            file_csv.write(','.join(baris) + '\n')
-
-        file_csv.close()
-        return True
-    else:
-        return False
-
-		
-def append_ke_csv(nama_file, new_data):
-    file_csv = open(nama_file, 'a')
-    for baris in new_data:
-        file_csv.write(','.join(baris) + '\n')
-    file_csv.close()
+import csv
 
 def baca_csv(nama_file):
     data = []
-    file_csv = open(nama_file, 'r')
-    for baris in file_csv:
-        data.append(baris.strip().split(','))
-    file_csv.close()
+    with open(nama_file, mode='r', newline='') as file_csv:
+        csv_reader = csv.reader(file_csv)
+        for baris in csv_reader:
+            data.append(baris)
     return data
+
+def baca_csv_sebagai_dict(nama_file):
+    data = []
+    with open(nama_file, mode='r', newline='') as file_csv:
+        csv_reader = csv.DictReader(file_csv)
+        for baris in csv_reader:
+            data.append(baris)
+    return data
+
+def tulis_csv(nama_file, data):
+    with open(nama_file, mode='w', newline='') as file_csv:
+        csv_writer = csv.writer(file_csv)
+        csv_writer.writerows(data)
+
+def tambah_ke_csv(nama_file, data_baru):
+    with open(nama_file, mode='a', newline='') as file_csv:
+        csv_writer = csv.writer(file_csv)
+        csv_writer.writerows(data_baru)
+
+def perbarui_baris_csv(nama_file, data, indeks_baris, baris_baru):
+    if 0 <= indeks_baris < len(data):
+        data[indeks_baris] = baris_baru
+        tulis_csv(nama_file, data)
+        return True
+    else:
+        return False
 
 def hapus_baris_csv(nama_file, data, indeks_baris):
     if 0 <= indeks_baris < len(data):
         del data[indeks_baris]
-        perbarui_data_csv(nama_file, data)
+        tulis_csv(nama_file, data)
         return True
     else:
         return False
 
-def perbarui_data_csv_berdasarkan_nilai_kolom(nama_file, data, nama_kolom, nilai, baris_baru):
+def perbarui_baris_csv_berdasarkan_nilai_kolom(nama_file, data, nama_kolom, nilai, baris_baru):
     header = data[0]
     indeks_kolom = header.index(nama_kolom)
 
@@ -44,7 +50,7 @@ def perbarui_data_csv_berdasarkan_nilai_kolom(nama_file, data, nama_kolom, nilai
         if baris[indeks_kolom] == nilai:
             data[data.index(baris)] = baris_baru
 
-    perbarui_data_csv(nama_file, data)
+    tulis_csv(nama_file, data)
 
 def hapus_baris_csv_berdasarkan_nilai_kolom(nama_file, data, nama_kolom, nilai):
     header = data[0]
@@ -55,8 +61,4 @@ def hapus_baris_csv_berdasarkan_nilai_kolom(nama_file, data, nama_kolom, nilai):
         if baris[indeks_kolom] != nilai:
             data_terbaru.append(baris)
 
-    perbarui_data_csv(nama_file, data_terbaru)
-
-
-
-
+    tulis_csv(nama_file, data_terbaru)
