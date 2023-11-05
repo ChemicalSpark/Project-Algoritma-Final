ebe4a1145a46de65720ad6d860158eac47e220c3
Oujireflect <oujireflect.me@gmail.com>
2023-11-05 10:25:14 +0700

    Tambah kategori selesai

--- kategori.py
+++ kategori.py
@@ -5,17 +5,16 @@ def list_kategori():
     df = pd.read_csv('database/kategori.csv')
     print(df)
 
-def tambah_kategori():
-    id_kategori = 1
-    print("Tambahkan Kategori!")
-    add_kategori = input("Masukkan kategori: ")
-    
-    
+def tambah_kategori(new_kategori):
+    with open('database/kategori.csv','r') as file:
+        data = [row.strip().split(',') for row in file.readlines()]
+        length = len(data)
+        data_temp = f"{length},{new_kategori}\n"
+    with open('database/kategori.csv','a') as add_kategori:
+        add_kategori.write(data_temp)
         
-
 def hapus_kategori():
     pass
-
 print("[KATEGORI]")
 print("Menu: ")
 print("""1. List Kategori
@@ -26,6 +25,8 @@ match user:
     case 1:
         list_kategori()
     case 2:
-        tambah_kategori()
+        print("Masukkan kategori baru!")
+        user = input("Kategori: ")
+        tambah_kategori(user)
     case 3:
         pass
