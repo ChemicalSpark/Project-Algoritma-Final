f088cca2c7541d9119d8fdca6acd7fe96869daee
DegenerateZan <noxindocraft@gmail.com>
2023-11-06 13:32:05 +0700

    removed complicated functions

--- core.py
+++ core.py
@@ -41,27 +41,3 @@ def hapus_baris_csv(nama_file, data, indeks_baris):
         return True
     else:
         return False
-<<<<<<< HEAD
-
-def perbarui_baris_csv_berdasarkan_nilai_kolom(nama_file, data, nama_kolom, nilai, baris_baru):
-    header = data[0]
-    indeks_kolom = header.index(nama_kolom)
-
-    for baris in data[1:]:
-        if baris[indeks_kolom] == nilai:
-            data[data.index(baris)] = baris_baru
-
-    tulis_csv(nama_file, data)
-
-def hapus_baris_csv_berdasarkan_nilai_kolom(nama_file, data, nama_kolom, nilai):
-    header = data[0]
-    indeks_kolom = header.index(nama_kolom)
-
-    data_terbaru = [header]
-    for baris in data[1:]:
-        if baris[indeks_kolom] != nilai:
-            data_terbaru.append(baris)
-
-    tulis_csv(nama_file, data_terbaru)
-=======
->>>>>>> ac17927823bbb76ef9396002257dab85071396f8
