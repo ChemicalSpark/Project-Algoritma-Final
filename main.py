e6746d7fbda2a461d6c55e7d53cd21ebce92b947
Oujireflect <oujireflect.me@gmail.com>
2023-11-07 16:44:32 +0700

    Menambah main.py dan ui

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
+def mainmenu():
     with open('ui/title.txt','r') as title:
         display = title.read()
         print(display)
-
-main()
+        user = int(input("| > Menu: "))
+        match user:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case _:
+                mainmenu()
+        
+    
+mainmenu()
