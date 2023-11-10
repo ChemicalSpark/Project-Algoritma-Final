cf9239d23c93f50c44a95cb1352ccc5c29ceff56
ChemicalSpark <rangga.pramudya23@gmail.com>
2023-11-10 16:38:32 +0700

    update part 3

--- login.py
+++ login.py
@@ -1,5 +1,4 @@
 import csv
-import sys
 
 def login():
     data_admin = []
@@ -11,22 +10,24 @@ def login():
             data_admin.append({"id": row[0], "username": row[1], "password": row[2]})
 
     attempts = 0
+    login_session = False
 
     print('='*51+'\n','Selamat Datang di Aplikasi Manajemen Perpustakaan'+'\n'+'='*51+'\n')
-    while attempts < 3:
+    while attempts < 3 and not login_session:
         username = input("Username: ")
         password = input("Password: ")
 
         for user in data_admin:
             if username == user['username'] and password == user['password']:
                 print('Login berhasil'+'\n')
-                return
-
-        print('Login Gagal'+'\n')
-        attempts += 1
-
-    print("Anda telah melebihi batas percobaan login.")
-    sys.exit()
+                login_session = True
+                break
+        if not login_session:
+            print('Login Gagal'+'\n')
+            attempts += 1
+    if attempts == 3 and not login_session:
+        print("Anda telah melebihi batas percobaan login.")
+        exit()
 
 if __name__ == "__main__":
     login()
