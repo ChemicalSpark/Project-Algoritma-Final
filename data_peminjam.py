8492815ba53c4f61db811ff57ab297246d91fe5c
ChemicalSpark <rangga.pramudya23@gmail.com>
2023-11-14 20:02:34 +0700

    entah kenapa hapusnya error

--- data_peminjam.py
+++ data_peminjam.py
@@ -1,12 +1,11 @@
 import csv
 import core
+import pandas as pd
 
 nama_file = 'database/data_peminjam.csv'
 
 def tulis_csv(data):
-    with open(nama_file, 'w', newline='') as file:
-        tulis = csv.writer(file)
-        tulis.writerows(data)
+    core.tulis_csv(nama_file, data)
 
 def tambah_baris_peminjam(nama, nim, telp):
     data = core.baca_csv(nama_file)
@@ -16,9 +15,37 @@ def tambah_baris_peminjam(nama, nim, telp):
     tulis_csv(data)
 
 def baca_baris_peminjam():
-    data = core.baca_csv(nama_file)
-    for baris in data:
-        print(baris)
+    # data = core.baca_csv(nama_file)
+    df = pd.read_csv(nama_file)
+    print(df.to_string(index=False))
+    # i = 1
+    # data = [["No.", "Nama", "NIM", "Telp", "id"]]
+                
+    # # var untuk ditampilkan
+    # data_tampil = [["No.", "Nama", "NIM", "Telp"]]
+                
+    # for baris in data:
+                    
+    #     # me skip baris kolom / header
+    #     if baris[0] == "id":
+    #         continue
+                    
+    #     data.append([i, baris[1], baris[2] , baris[3], baris[0]])
+                    
+    #     data_tampil.append([i, baris[1], baris[2] , baris[3]])
+                            
+    #     i += 1
+                    
+    # # membuat dataframe dan me-set kolom custom
+    # df = pd.DataFrame(data_tampil[1:], columns=['No.', 'Nama', 'NIM','Telp'])
+
+    # # untuk mengabaikan index bawaan pandas
+    # output = df.to_string(index=False)
+    # output = output.split("\n")
+    # hasil = ""
+    # for i in output:
+    #     hasil += " "*23 + i + "\n"    
+    # print(hasil)
 
 def perbarui_baris_peminjam(id, nama, nim, telp):
     data = core.baca_csv(nama_file)
@@ -31,6 +58,9 @@ def perbarui_baris_peminjam(id, nama, nim, telp):
     tulis_csv(data)
 
 def hapus_baris_peminjam(id):
+    # user = input('Masukkan ID yang akan dihapus: ')
+    # row_delete = user
+    # df = data.drop(row_delete)
     data = core.baca_csv(nama_file)
     index_baris = core.cari_index_dengan_id_list(data, id)
     if index_baris == 0:
@@ -57,22 +87,35 @@ def aksi_peminjam():
             core.clear()
             print("Data saat ini:")
             baca_baris_peminjam()
-            print("\n")
+            
         elif pilih == 3:
+            print("Data saat ini:")
+            baca_baris_peminjam()
             id = input("Masukkan ID data yang akan diperbarui: ")
             data = core.cari_id_list(core.baca_csv(nama_file), id)
             if data == False:
                 print("Data Tidak ada"+'\n')
+                baca_baris_peminjam()
                 core.clear()
             else:
                 print("Nama lama :", data[1])
                 nama = input("Masukkan Nama yang baru : ")
+                match nama:
+                        case _:
+                            pass
                 print("NIM lama :", data[2])
                 no = input("Masukkan NIM yang baru : ")
+                match no:
+                    case _:
+                        pass
                 print("Nomor Telepon lama :", data[3])
                 telp = input("Masukkan Nomor Telepon yang baru : ")
+                match telp:
+                    case _:
+                        pass
                 perbarui_baris_peminjam(id, nama, no, telp)
                 print("Data telah diperbarui."+'\n')
+                enter  = print("Klik enter untuk meneruskan")
                 core.clear()
         elif pilih == 4:
             id = input("Masukkan ID data yang akan dihapus: ")
@@ -80,9 +123,11 @@ def aksi_peminjam():
             if confirm == 'y':
                 hapus_baris_peminjam(id)
                 print("Data telah dihapus."+'\n')
+                enter  = print("Klik enter untuk meneruskan")
                 core.clear()
             elif confirm == 'n':
                 print('Data batal dihapus'+'\n')
+                enter  = print("Klik enter untuk meneruskan")
                 core.clear()
         elif pilih == 9:
             print("Kembali")
@@ -95,9 +140,9 @@ def aksi_peminjam():
             core.clear()
 
 if __name__ == "__main__":
+    aksi_peminjam()
     tulis_csv()
     tambah_baris_peminjam()
     baca_baris_peminjam()
     perbarui_baris_peminjam()
     hapus_baris_peminjam()
-    aksi_peminjam()
