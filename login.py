0adc6fb4577ff99dc803d833bbaaa1710b505b4d
Rangg <142792905+ChemicalSpark@users.noreply.github.com>
2023-12-06 18:45:17 +0700

    apdet sesi dan percobaan

--- login.py
+++ login.py
@@ -19,17 +19,14 @@ def login():
             data_admin.append({"id": row[0], "username": row[1], "password": row[2], "role": row[3]})
 
     #Count kesempatan untuk login
-    attempts = 3
-    # -
-    login_session = False
+    percobaan = 3
+    sesi_login = False
     
     # Main Program
-    while attempts > 0 and not login_session:
+    while percobaan > 0 and not sesi_login:
         # Menampilkan UI CLI dari login session
         with open('ui/login.txt','r') as login_ui:
-            display = login_ui.read()
-            print(display)
-
+            print(login_ui.read())
         # Meminta input dari user berupa username dan password
         username = input("| Username: ")
         password = getpass.getpass(prompt="| Password: ")
@@ -38,13 +35,13 @@ def login():
             # Kondisi benar jika input sesuai dengan database
             if username == user['username'] and password == user['password'] and user['role']:
                 # UI Login Berhasil
-                greeting = f"Selamat Datang {username.title()} dengan role {user['role'].title()}"
+                salam = f"Selamat Datang {username.title()} dengan role {user['role'].title()}"
                 print('+' + '='*83 + '+')
                 print('|' + '[ LOGIN SUCCESFUL ]'.center(83) + '|')
-                print('|' + greeting.center(83) + '|')
+                print('|' + salam.center(83) + '|')
                 print('+' + '='*83 + '+')
                 # setelah masuk, sesi login true
-                login_session = True
+                sesi_login = True
                 global SESSION_GLOBAL
                 SESSION_GLOBAL = user
                 # Pause sebelum di clear
@@ -52,15 +49,15 @@ def login():
                 core.clear()
                 break
         # -
-        if not login_session:
+        if not sesi_login:
             # UI Login Gagal
             print('+' + '='*83 + '+')
             print('|' + '[ LOGIN FAILED ]'.center(83) + '|')
             print('|' + 'Username atau Password salah!'.center(83) + '|')
             print('+' + '='*83 + '+')
-            attempts -= 1
+            percobaan -= 1
             #  - 
-            if attempts == 0 and not login_session:
+            if percobaan == 0 and not sesi_login:
                 # UI Login gagal
                 print('+' + '='*83 + '+')
                 print('|' + '[ LOGIN SESSION ATTEMPT OUT ]'.center(83) + '|')
